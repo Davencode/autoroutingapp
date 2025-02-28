09f275b3246d777aba824748e3d544d0ef5ffb70
DavideBelardi <davidebelardi1994@gmail.com>
2025-02-28 15:49:12 +0100

    Implementation of Easy Localization to translate the apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:autoroutingapp/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+      EasyLocalization(
+        supportedLocales: const[Locale('it'), Locale('en')],
+        path: 'assets/languages',
+        fallbackLocale: const Locale('it'),
+        child: MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +22,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      // hook up router to MaterialApp
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       routerConfig: _appRouter.config(),
     );
   }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,23 +1,48 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 import '../routes/app_router.dart';
 
 @RoutePage()
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isEn = true;
+
+  void changeLanguages() {
+    setState(() {
+      isEn = !isEn;
+      isEn ? context.setLocale(const Locale('it')) : context.setLocale(const Locale('en'));
+      debugPrint(isEn.toString());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home")),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.router.push(DetailsRoute());
-          },
-          child: const Text("Go to Details"),
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                context.router.push(DetailsRoute());
+              },
+              child: Text('goDetails'.tr()),
+            ),
+          ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {
+              changeLanguages();
+            }, child: Text('Translate')
+            )
+        ],
       ),
     );
   }
